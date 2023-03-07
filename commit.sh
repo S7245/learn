2489301a0781041b397669b78bac580077707205
S7245 <samlau0401@gmail.com>
2023-03-07 10:40:50 +0800

    更新

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 
 git pull
 git add -A
-git commit -m '更新内容'
-git push --force origin master
-# git push -u origin main
+git commit -m '更新'
+git push 
+# git push -f git@github.com:samlau0401/known.git main
 # chmod 777 /commit.sh  #使脚本具有执行权限
